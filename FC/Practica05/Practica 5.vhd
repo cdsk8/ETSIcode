022a05b4d35460d6a02074b70a4ee110b40ae8ed
BorjaLive <borjainlive@gmail.com>
2018-12-11 22:21:58 +0100

    Ejercicio 5

---
+++ FC/Practica05/Practica 5.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Enginer:        
+-- Create Date:    22:04:37 11/12/2018
+-- Module Name:    Practica5 - Arquitectura
+-- Project Name:   Practica 5.vme
+-- Description:    
+--
+-- Created with VHDL ME. Parser by B0vE, powred by Temis (De mesa)
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+Entity Practica5 is
+	Port(C0 : In STD_LOGIC;
+		  C1 : In STD_LOGIC;
+		  C2 : In STD_LOGIC;
+		  C3 : In STD_LOGIC;
+		  A : Out STD_LOGIC;
+		  I : Out STD_LOGIC;
+		  J : Out STD_LOGIC);
+End Practica5;
+
+Architecture Arquitectura of Practica5 is
+	 Signal entrada :  STD_LOGIC_VECTOR(3 downto 0);
+	 Signal entrada_int :  INTEGER range 0 to 15;
+Begin
+	A <= ((Not C2) And (Not C3)) Or (C2 And C3) Or (C0 And C1 And (Not C2)) Or ((Not C0) And (Not C1) And C2);
+	entrada <= C3&C2&C1&C0;
+
+	I <= '1' When entrada = "0001" else
+		  '1' When entrada = "0010" else
+		  '1' When entrada = "0100" else
+		  '1' When entrada = "0111" else
+		  '1' When entrada = "1000" else
+		  '1' When entrada = "1011" else
+		  '1' When entrada = "1101" else
+		  '1' When entrada = "1110" else
+		  '0';
+
+	entrada_int <= CONV_INTEGER(entrada);
+
+	With entrada_int select
+	J <= '1' When 0|1|3|7|8|12|14|15,
+		  '0' When others;
+End Arquitectura;
---
+++ FC/Practica05/Practica5_BranchTest.vhd
@@ -0,0 +1,92 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY Practica5Test IS
+END Practica5Test;
+ 
+ARCHITECTURE behavior OF Practica5Test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Practica5
+    PORT(
+         C0 : IN  std_logic;
+         C1 : IN  std_logic;
+         C2 : IN  std_logic;
+         C3 : IN  std_logic;
+         A : OUT  std_logic;
+         I : OUT  std_logic;
+         J : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal C0 : std_logic := '0';
+   signal C1 : std_logic := '0';
+   signal C2 : std_logic := '0';
+   signal C3 : std_logic := '0';
+
+ 	--Outputs
+   signal A : std_logic;
+   signal I : std_logic;
+   signal J : std_logic;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Practica5 PORT MAP (
+          C0 => C0,
+          C1 => C1,
+          C2 => C2,
+          C3 => C3,
+          A => A,
+          I => I,
+          J => J
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+	
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+		C3 <= '0';C2 <= '0';C1 <= '0';C0 <= '0';
+		Wait for 10 ms;
+		C3 <= '0';C2 <= '0';C1 <= '0';C0 <= '1';
+		Wait for 10 ms;
+		C3 <= '0';C2 <= '0';C1 <= '1';C0 <= '0';
+		Wait for 10 ms;
+		C3 <= '0';C2 <= '0';C1 <= '1';C0 <= '1';
+		Wait for 10 ms;
+		C3 <= '0';C2 <= '1';C1 <= '0';C0 <= '0';
+		Wait for 10 ms;
+		C3 <= '0';C2 <= '1';C1 <= '0';C0 <= '1';
+		Wait for 10 ms;
+		C3 <= '0';C2 <= '1';C1 <= '1';C0 <= '0';
+		Wait for 10 ms;
+		C3 <= '0';C2 <= '1';C1 <= '1';C0 <= '1';
+		Wait for 10 ms;
+		C3 <= '1';C2 <= '0';C1 <= '0';C0 <= '0';
+		Wait for 10 ms;
+		C3 <= '1';C2 <= '0';C1 <= '0';C0 <= '1';
+		Wait for 10 ms;
+		C3 <= '1';C2 <= '0';C1 <= '1';C0 <= '0';
+		Wait for 10 ms;
+		C3 <= '1';C2 <= '0';C1 <= '1';C0 <= '1';
+		Wait for 10 ms;
+		C3 <= '1';C2 <= '1';C1 <= '0';C0 <= '0';
+		Wait for 10 ms;
+		C3 <= '1';C2 <= '1';C1 <= '0';C0 <= '1';
+		Wait for 10 ms;
+		C3 <= '1';C2 <= '1';C1 <= '1';C0 <= '0';
+		Wait for 10 ms;
+		C3 <= '1';C2 <= '1';C1 <= '1';C0 <= '1';
+		Wait for 10 ms;
+
+      wait;
+   end process;
+
+END;
